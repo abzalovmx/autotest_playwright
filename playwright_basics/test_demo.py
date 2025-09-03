a5dcf1c63316160025ddecb330cdb70e2048699f
Mirzaali Abzalov <mirzaali.abzalov@fido.uz>
2025-09-03 17:47:59 +0500

    add some test

--- playwright_basics/test_demo.py
+++ playwright_basics/test_demo.py
@@ -3,34 +3,47 @@ import pytest
 from playwright.sync_api import sync_playwright, Page, expect
 
 
-# 25-01
+# 37-00
 
 @pytest.fixture(scope="session")
 def browser():
     with sync_playwright() as p:
-        browser = p.chromium.launch(proxy={"server": "http://inet.fido.uz:3128"})
+        browser = p.chromium.launch(
+            headless=False,  # headed режим
+            slow_mo=200,     # замедление для наглядности
+            proxy={"server": "http://inet.fido.uz:3128"},
+            args=["--disable-blink-features=AutomationControlled"]
+        )
         yield browser
         browser.close()
 
+
 @pytest.fixture()
-def page(browser):
-    page = browser.new_page()
+def context(browser):
+    context = browser.new_context(
+        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                   "(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+        viewport={"width": 1280, "height": 800}
+    )
+    yield context
+    context.close()
+
+
+@pytest.fixture()
+def page(context):
+    page = context.new_page()
     yield page
     page.close()
 
-def test_google():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, args=["--disable-blink-features=AutomationControlled"])
-        context = browser.new_context(
-            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                       "(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-            viewport={"width": 1280, "height": 800}
-        )
-        page = context.new_page()
+
+def test_google(page: Page):
+    page.goto("https://www.google.com/")
+    page.fill("textarea[name='q']", "cat")
+    page.press("textarea[name='q']", "Enter")
+    expect(page).to_have_title(re.compile("^cat"))
 
 
-        page.goto("https://www.google.com/")
-        page.fill("textarea[name='q']", "cat")
-        page.press("textarea[name='q']", "Enter")
-        expect(page).to_have_title(re.compile('^cat'))
-        browser.close()
+def test_by_role(page: Page):
+    page.goto("https://magento.softwaretestingboard.com/")
+    page.get_by_role("menuitem", name="What's New").click()
+    page.get_by_role("link", name="Search Terms").click()

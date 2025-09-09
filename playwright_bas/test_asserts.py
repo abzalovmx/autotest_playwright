ab0b93b08ee98e90436fa47bd84a433f02956e70
abzalovmx <abzalov484@gmail.com>
2025-09-09 22:00:10 +0500

    add

--- playwright_bas/test_asserts.py
+++ playwright_bas/test_asserts.py
@@ -3,15 +3,15 @@ import pytest
 from playwright.sync_api import sync_playwright, Page, expect
 from time import sleep
 
-# 44-35
+# 53-13
 
 @pytest.fixture(scope="session")
 def browser():
     with sync_playwright() as p:
         browser = p.chromium.launch(
-            headless=False,  # headed режим
-            slow_mo=200,     # замедление для наглядности
-            proxy={"server": "http://inet.fido.uz:3128"},
+            headless=False,
+            slow_mo=200,
+            # proxy={"server": "http://inet.fido.uz:3128"},
             args=["--disable-blink-features=AutomationControlled"]
         )
         yield browser
@@ -36,5 +36,35 @@ def page(context):
     page.close()
 
 
-def test_():
-    pass
+def test_visible(page: Page):
+    page.goto("https://www.qa-practice.com/elements/input/simple")
+    reqs = page.locator('#req_text')
+    expect(reqs).not_to_be_visible()
+    page.locator('#req_header').click()
+    expect(reqs).to_be_visible()
+
+def test_enabled_and_select(page: Page):
+    page.goto('https://www.qa-practice.com/elements/button/disabled')
+    button = page.locator('#submit-id-submit')
+    expect(button).to_be_disabled()
+    page.locator('#id_select_state').select_option('enabled')
+    expect(button).to_be_enabled()
+    expect(button).to_have_text('Submit')
+
+def test_value(page: Page):
+    txt = 'qwerty'
+    page.goto('https://www.qa-practice.com/elements/input/simple')
+    input_field = page.locator('#id_text_string')
+    input_field.fill(txt)
+    expect(input_field, f'input value is not {txt}').to_have_value(txt)
+
+
+def test_sorting_and_waits(page: Page):
+    page.goto('https://magento.softwaretestingboard.com/hero-hoodie.html')
+    greet = page.locator('.greet.welcome').locator('nth=0')
+    expect(greet).not_to_be_empty()
+    first_man = page.locator('.product-item-link').locator('nth=0')
+    print(first_man.inner_text())
+    page.locator('#sorter').locator('nth=0)').select_option('Price')
+    expect(page).to_have_url(re.compile('price'))
+    print(first_man.inner_text())
